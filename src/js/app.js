a8217ca83171b0aba5fcac4dbc76ea6c9b0336d7
Zept0n <104791727+Zept0n@users.noreply.github.com>
2023-01-15 10:47:06 +0200

    feat:adding data-price attr to every product

--- src/js/app.js
+++ src/js/app.js
@@ -3,5 +3,15 @@ import "../scss/app.scss";
 window.addEventListener("DOMContentLoaded", () => {
   // This block will be executed once the page is loaded and ready
 
+  // Get all elements with the class 'product'
+  var products = document.getElementsByClassName("product");
 
+  // Loop through each product
+  for (var i = 0; i < products.length; i++) {
+    // Get the price of the product
+    var price = products[i].getElementsByClassName("price")[0].textContent;
+
+    // Add the 'data-price' attribute to the product element
+    products[i].setAttribute("data-price", price);
+  }
 });
